8aff92d9028dfe45fb432dc2bc513ad68c262ef2
Mustafa Natur <mustafa.natur@mail.ru>
2023-02-23 15:52:21 +0300

    develop: custom table view cell

---
+++ MyCityApp/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyCityApp
+//
+//  Created by Мустафа Натур on 23.02.2023.
+//
+
+import Foundation
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+
+
+}
