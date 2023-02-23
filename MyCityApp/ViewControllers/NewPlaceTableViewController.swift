73b0d8e59c78566746489cfc2180645724879314
Mustafa Natur <mustafa.natur@mail.ru>
2023-02-23 19:03:25 +0300

    develop: added photo and camera process

--- MyCityApp/ViewControllers/NewPlaceTableViewController.swift
+++ MyCityApp/ViewControllers/NewPlaceTableViewController.swift
@@ -11,19 +11,37 @@ class NewPlaceTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        imageOfPlace.layer.cornerRadius = 15
     }
 
+    @IBOutlet weak var mainImageView: UIView!
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if (indexPath.row == 0) {
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             
+            let photo = UIAlertAction(title: "Choose from gallery", style: .default) {
+                _ in self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            let camera = UIAlertAction(title: "Make photo", style: .default) {
+                _ in self.chooseImagePicker(source: .camera)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
+
         } else {
             view.endEditing(true)
         }
     }
-    
-    
-
+   
 }
 
 // MARK: Text field delegate
@@ -32,5 +50,24 @@ extension NewPlaceTableViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+extension NewPlaceTableViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        guard UIImagePickerController.isSourceTypeAvailable(source) else { return }
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = source
+        present(imagePicker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill
+        imageOfPlace.clipsToBounds = true
+        mainImageView.backgroundColor = .systemBackground
+        dismiss(animated: true)
     }
 }
