3efcf15221edfe0a2ffbdf7b030c35ffb612324c
Mustafa Natur <mustafa.natur@mail.ru>
2023-02-24 16:16:37 +0300

    develop: added sorting by date or name and reverse sorting

--- MyCityApp/Model/Model.swift
+++ MyCityApp/Model/Model.swift
@@ -15,4 +15,5 @@ class Place: Object {
     @objc dynamic var location:String?
     @objc dynamic var type:String?
     @objc dynamic var imageData: Data?
+    @objc dynamic var date = Date()
 }
--- MyCityApp/Support/SceneDelegate.swift
+++ MyCityApp/Support/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,6 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        let config = Realm.Configuration(
+            schemaVersion: 1)
+        // Use this configuration when opening realms
+        Realm.Configuration.defaultConfiguration = config
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
