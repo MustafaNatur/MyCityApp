ed571c71b415706d074dcfee451c9cdcf9325cd4
Mustafa Natur <mustafa.natur@mail.ru>
2023-02-24 15:07:26 +0300

    develop: added edit vc and prepared for sorting

--- MyCityApp/ViewControllers/MainTableViewController.swift
+++ MyCityApp/ViewControllers/MainTableViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 import RealmSwift
 
-class MainTableViewController: UITableViewController {
+class MainTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var places: Results<Place>!
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,25 +21,44 @@ class MainTableViewController: UITableViewController {
         
         places = realm.objects(Place.self)
     }
-//    
+ 
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
         if let vc = segue.source as? NewPlaceTableViewController {
             vc.savePlace()
         }
         tableView.reloadData()
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toEdit" {
+            if let vc = segue.destination as? NewPlaceTableViewController {
+                vc.editPlace = places[tableView.indexPathForSelectedRow!.row]
+            }
+        }
+    }
 
     // MARK: - Table view data source
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return places.isEmpty ? 0:places.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         cell.setUp(place: places[indexPath.row])
         return cell
     }
     
     // MARK: - Table view delegate
+    
+     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let place = places[indexPath.row]
+        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (_, _)in
+            StorageManager.deleteObject(place)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        
+        return [deleteAction]
+    }
 
 }
--- MyCityApp/ViewControllers/NewPlaceTableViewController.swift
+++ MyCityApp/ViewControllers/NewPlaceTableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class NewPlaceTableViewController: UITableViewController {
     
     var imageChanged = false
+    var editPlace:Place?
 
     @IBOutlet weak var mainImageView: UIView!
     @IBOutlet weak var imageOfPlace: UIImageView!
@@ -20,23 +21,19 @@ class NewPlaceTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         imageOfPlace.layer.cornerRadius = 15
-        
         saveButton.isEnabled = false
-        
         nameTextField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        changeViewForEdit()
     }
     
-    @IBAction func cancleButtonPessedr(_ sender: UIBarButtonItem) {
+    @IBAction func cancleButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
     
     func savePlace() {
         let image = imageChanged ? imageOfPlace.image:UIImage(named: "defaultImage")
-        
         let imageData = image?.pngData()
-        
         let newPlace = Place()
         
         newPlace.name = nameTextField.text!
@@ -44,7 +41,36 @@ class NewPlaceTableViewController: UITableViewController {
         newPlace.type = typeTextField.text
         newPlace.imageData = imageData
         
-        StorageManager.saveObject(newPlace)
+        if editPlace != nil {
+            try! realm.write {
+                editPlace?.name = newPlace.name
+                editPlace?.location = newPlace.location
+                editPlace?.type = newPlace.type
+                editPlace?.imageData = newPlace.imageData
+            }
+        } else {
+            StorageManager.saveObject(newPlace)
+        }
+    }
+    
+    func changeViewForEdit() {
+        if let place = editPlace {
+            setupNavigationController()
+            nameTextField.text = place.name
+            locationTextField.text = place.location
+            typeTextField.text = place.type
+            imageOfPlace.image = UIImage(data: place.imageData!)
+            imageOfPlace.contentMode = .scaleAspectFill
+            imageOfPlace.clipsToBounds = true
+            mainImageView.backgroundColor = .systemBackground
+            imageChanged = true
+            saveButton.isEnabled = true
+            title = place.name
+        }
+    }
+    
+    func setupNavigationController() {
+        navigationItem.leftBarButtonItem = nil
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -52,7 +78,6 @@ class NewPlaceTableViewController: UITableViewController {
             
             let cameraIcon = UIImage(systemName: "camera")
             let photoIcon = UIImage(systemName: "photo")
-            
             let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             
             let photo = UIAlertAction(title: "Choose from gallery", style: .default) {
@@ -60,20 +85,18 @@ class NewPlaceTableViewController: UITableViewController {
             }
             photo.setValue(photoIcon, forKey: "image")
             photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
-
             
             let camera = UIAlertAction(title: "Make photo", style: .default) {
                 _ in self.chooseImagePicker(source: .camera)
             }
             camera.setValue(cameraIcon, forKey: "image")
             camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
-
             
             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
-            
             actionSheet.addAction(camera)
             actionSheet.addAction(photo)
             actionSheet.addAction(cancel)
+            
             present(actionSheet, animated: true)
 
         } else {
@@ -96,9 +119,11 @@ extension NewPlaceTableViewController: UIImagePickerControllerDelegate, UINaviga
     func chooseImagePicker(source: UIImagePickerController.SourceType) {
         guard UIImagePickerController.isSourceTypeAvailable(source) else { return }
         let imagePicker = UIImagePickerController()
+        
         imagePicker.delegate = self
         imagePicker.allowsEditing = true
         imagePicker.sourceType = source
+        
         present(imagePicker, animated: true)
     }
     
@@ -108,6 +133,7 @@ extension NewPlaceTableViewController: UIImagePickerControllerDelegate, UINaviga
         imageOfPlace.clipsToBounds = true
         mainImageView.backgroundColor = .systemBackground
         imageChanged = true
+        
         dismiss(animated: true)
     }
     
